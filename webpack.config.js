278a71fa03e9e650db143d4f7db53396b58137c5
Dmitry <balakovd@gmail.com>
2019-11-23 16:15:07 +0300

    Add scripts: docker & build

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,8 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  target: 'web',
+  mode: 'production',
   entry: {
     index: [
       'babel-polyfill', 
